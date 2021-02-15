e886a63c6e38d9d716e81f5fb2aef8b45863700a
giorgi <gogsadze_g@bk.ru>
2021-02-16 00:14:13 +0400

    lab9

--- index.js
+++ index.js
@@ -14,3 +14,67 @@ axios.get('https://jsonplaceholder.typicode.com/todos/1')
 .catch(function (error) {
   console.log(error);
 });
+const axios = require('axios');
+
+let postsTable = document.querySelector('#posts-table');
+let commentsTable = document.querySelector('#comments-table');
+let getPostsButton = document.querySelector('#get-posts');
+
+getPostsButton.addEventListener('click', () => {
+  getPosts();
+})
+
+function getPosts() {
+  let userId = document.getElementById('user-id').value;
+  fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+    .then(response => response.json())
+    .then(json => displayPosts(json));
+}
+
+function displayPosts(posts) {
+  document.querySelectorAll('table').forEach(table => table.classList.remove('active'));
+  commentsTable.querySelector('tbody').innerHTML = '';
+  postsTable.querySelector('tbody').innerHTML = '';
+  posts.forEach(post => {
+    let tr = document.createElement('tr');
+    let rowSingle = `
+      <td>${post.title}</td>
+      <td>${post.body}</td>
+      <td>
+        <button id='${post.id}' class="get-comments">see comments</button>
+      </td>
+    `
+    tr.innerHTML = rowSingle;
+    postsTable.querySelector('tbody').appendChild(tr);
+  })
+  document.querySelectorAll('.get-comments').forEach(button => button.addEventListener('click', (e) => {
+    getComments(e.target);
+  }));
+  document.querySelector('#posts-table').classList.add('active');
+}
+
+function getComments(btn) {
+  let id = btn.getAttribute('id');
+  axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+    .then((response) => {
+      displayComments(response.data);
+      document.querySelectorAll('#posts-table tr').forEach(tr => tr.classList.remove('active'));
+      btn.closest('tr').classList.add('active');
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function displayComments(comments) {
+  commentsTable.classList.add('active');
+  commentsTable.querySelector('tbody').innerHTML = '';
+  comments.forEach(comment => {
+    let tr = document.createElement('tr');
+    let rowSingle = `
+      <td>${comment.body}</td>
+    `
+    tr.innerHTML = rowSingle;
+    commentsTable.querySelector('tbody').appendChild(tr);
+  })
+}
